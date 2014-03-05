52cf2c71ddbdcdf2bb2d64e651fd307f2f1a463c
SubhashShah-BTC <subhash.shah@beaverlogic.com>
2014-03-05 11:53:25 +0530

    Salesforce create event

--- lib/utilities/salesforce/salesforce_api_util.rb
+++ lib/utilities/salesforce/salesforce_api_util.rb
@@ -10,10 +10,19 @@ module Utilities
       end
 
       def get_events(options = {})
-        client.authenticate({:token => options[:token], :instance_url => options[:instance_url],
-                            :refresh_token => options[:refresh_token]})
+        authenticate(options)
         client.materialize("Event")
         events = Event.all
+      end
+
+      def create_event(options = {}, data)
+        authenticate(options)
+        client.create("Event", data)
+      end
+
+      def authenticate(options)
+        client.authenticate({:token => options[:token], :instance_url => options[:instance_url],
+                            :refresh_token => options[:refresh_token]})
       end
       
     end
